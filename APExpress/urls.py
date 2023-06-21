e082173701e3016afbe1363bda75ad06e2eab420
jnm31415 <131339549+jnm31415@users.noreply.github.com>
2023-06-21 11:31:43 +0200

    Update urls.py

--- APExpress/urls.py
+++ APExpress/urls.py
@@ -19,8 +19,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Aufträge.urls'))
+    path('', include('Aufträge.urls')),
+    path('admin/', admin.site.urls)
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
