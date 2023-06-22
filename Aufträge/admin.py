dac6ae5f5eb281a13e443aa737da5c32cde7f111
jnm31415 <131339549+jnm31415@users.noreply.github.com>
2023-06-22 15:45:48 +0200

    Update admin.py

--- Aufträge/admin.py
+++ Aufträge/admin.py
@@ -6,7 +6,8 @@ from .models import Adressen
 from .models import Fahrer
 from .models import Rechnung
 from .models import Help
-
+from django.contrib.admin import AdminSite
+'''
 # Register your models here.
 admin.site.register(Auftragspositionen)
 admin.site.register(Auftrag)
@@ -15,3 +16,21 @@ admin.site.register(Rechnungsempfänger)
 admin.site.register(Fahrer)
 admin.site.register(Help)
 admin.site.register(Rechnung)
+'''
+
+
+
+class MyAdminSite(AdminSite):
+
+    def get_urls(self):
+        urlpatterns = super().get_urls()
+        for model, model_admin in self._registry.items():
+            urlpatterns += [
+                path('%s/' % (model._meta.model_name), include(model_admin.urls)),
+            ]
+        return urlpatterns
+
+
+my_admin = MyAdminSite('My Admin')
+
+my_admin.register(Auftrag)
