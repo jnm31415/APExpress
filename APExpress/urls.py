5541006e20d397bdc0c36182cb12663b85f1c744
jnm31415 <131339549+jnm31415@users.noreply.github.com>
2023-06-27 17:00:01 +0200

    Update urls.py

--- APExpress/urls.py
+++ APExpress/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from Aufträge.adminapps import admin_view
 
 
 urlpatterns = [
