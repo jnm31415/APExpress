ce5a9ef0cace5b5b1219e1efdfafb607353184a9
jnm31415 <131339549+jnm31415@users.noreply.github.com>
2023-06-27 14:29:56 +0200

    Update settings.py

--- APExpress/settings.py
+++ APExpress/settings.py
@@ -36,6 +36,7 @@ DATABASE_URL = "postgres://default:J7Efao6cQrUC@ep-patient-butterfly-717518.eu-c
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -45,7 +46,7 @@ INSTALLED_APPS = [
     'django_filters',
     'Aufträge.apps',
 ]
-#'django.contrib.admin',
+#
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
