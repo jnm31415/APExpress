202ba77d3b5ac9335e2992bb79df6d224992370b
jnm31415 <131339549+jnm31415@users.noreply.github.com>
2023-06-27 14:39:54 +0200

    Update apps.py

--- Aufträge/apps.py
+++ Aufträge/apps.py
@@ -4,6 +4,10 @@ from django.contrib.admin import apps
 class AufträgeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Aufträge'
+    verbose_name = 'Auftraege'
 
+
+'''
 class MyAdminConfig(apps.AdminConfig):
     default_site = 'Aufträge.admin.MyAdminSite'
+'''
