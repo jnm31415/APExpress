41f3b97685c92754d22b34adad0fc7651197df69
jnm31415 <131339549+jnm31415@users.noreply.github.com>
2023-06-27 16:55:34 +0200

    Update admin.py

--- Aufträge/admin.py
+++ Aufträge/admin.py
@@ -9,7 +9,7 @@ from .models import Rechnung
 from .models import Help
 
 
-'''
+
 class MyAdminSite(admin.AdminSite):
     def get_urls(self):
         urlpatterns = super().get_urls()
@@ -18,9 +18,17 @@ class MyAdminSite(admin.AdminSite):
                 path('%s/' % (model._meta.model_name), include(model_admin.urls)),
             ]
         return urlpatterns
+
+admin_view = MyAdminSite()
+admin_view.register(Auftrag)
+admin_view.register(Auftragspositionen)
+admin_view.register(Rechnungsempfänger)
+admin_view.register(Adressen)
+admin_view.register(Fahrer)
+admin_view.register(Rechnung)
+admin_view.register(Help)
+
 '''
-
-
 admin.site.register(Auftragspositionen)
 admin.site.register(Auftrag)
 admin.site.register(Adressen)
@@ -28,7 +36,7 @@ admin.site.register(Rechnungsempfänger)
 admin.site.register(Fahrer)
 admin.site.register(Help)
 admin.site.register(Rechnung)
-
+'''
 
 
 
