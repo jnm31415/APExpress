82ec9501a9ab0b42ee7cb7c530332fdf7b83167b
jnm31415 <131339549+jnm31415@users.noreply.github.com>
2023-06-27 15:01:01 +0200

    Update apps.py

--- Aufträge/apps.py
+++ Aufträge/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 class AufträgeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Aufträge'
-    verbose_name = 'Auftraege'
+    
 
 
 
